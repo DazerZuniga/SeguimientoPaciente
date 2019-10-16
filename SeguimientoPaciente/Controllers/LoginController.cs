e321c4cbcfd73efe47144af5a3bce0eba6280ad7
DazerZuniga <desingjz@gmail.com>
2019-10-16 16:37:25 -0300

    proyecto hasta la fecha

---
+++ SeguimientoPaciente/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeguimientoPaciente.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+		
+		[HttpPost]
+		public ActionResult validar(int e , string t) {
+
+			/*var usuario = db.usuario.where(x=> x.pass == t && x.correo == e );
+			var tipo = db.tipo.where(x => x.fkusu == usuario.id);
+			if (usuario != null) {
+				if (tipo.descripcion == 1)
+				{
+
+
+				}
+
+			}*/
+			return RedirectToAction("index", "Home");
+		}
+    }
+}
